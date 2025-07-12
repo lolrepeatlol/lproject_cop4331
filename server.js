4d2b3c2c2488dbac446051bbd98aec189a6d6030
Jose Ojeda <144283545+OuttaTime76@users.noreply.github.com>
2025-07-12 00:41:45 -0400

    Added Server.js pathing

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(bodyParser.json());
 
