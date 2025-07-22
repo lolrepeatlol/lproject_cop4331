18493d7b80ec92b08625a2c5d3dd21a8a40a86bb
Jose Ojeda <144283545+OuttaTime76@users.noreply.github.com>
2025-07-22 12:56:24 -0400

    Server changes for postman

--- server.js
+++ server.js
@@ -17,18 +17,4 @@ client.connect();
 // Initialize API routes
 const api = require('./api.js');
 api.setApp(app, client);
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
-
 app.listen(5000);
